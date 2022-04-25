036856cf69543217a24a157e76a32a4835ae0d18
eexpress <eexpress@163.com>
2022-04-25 13:04:32 +0800

    icon看来被什么事件覆盖了

--- extension.js
+++ extension.js
@@ -13,7 +13,7 @@ const _ = ExtensionUtils.gettext;
 function lg(s) { log("===" + _domain + "===>" + s); }
 
 //~ const tmpfile = "/tmp/qrcode.png";
-const tmpfile = "/home/eexpss/qrcode.png";
+//~ const tmpfile = "/home/eexpss/qrcode.png";
 
 const Indicator = GObject.registerClass(
 	class Indicator extends PanelMenu.Button {
@@ -21,19 +21,34 @@ const Indicator = GObject.registerClass(
 			super._init(0.0, _(Me.metadata['name']));
 			this._clipboard = St.Clipboard.get_default();
 			this.lasttext = '';
+			const tmpfile = GLib.get_home_dir() + "/qrcode.png";
 
 			const micon = new St.Icon({ icon_name : "edit-find-symbolic", style_class : 'system-status-icon' });
 			this.add_child(micon);
+
+			//~ const tmpfile = GLib.get_home_dir() + "/qrcode.png";
 			this.mqrcode = new PopupMenu.PopupBaseMenuItem();
+			this.mqrcode.set_track_hover = true;
 			this.icon = new St.Icon({
-				icon_name : "edit-find-symbolic",
+				//~ icon_name : "edit-find-symbolic",
+				gicon: Gio.icon_new_for_string(tmpfile),
 				icon_size: 256,
-				});
+				track_hover : true,
+			});
+
+			this.icon.connect("enter-event", (widget) => {
+			widget.gicon = Gio.icon_new_for_string(tmpfile);
+			});
+			this.icon.connect("leave-event", (widget) => {
+			widget.gicon = Gio.icon_new_for_string(tmpfile);
+			});
+
 			this.mqrcode.actor.add_child(this.icon);
 			this.menu.addMenuItem(this.mqrcode);
 
 			this.menu.connect('open-state-changed', (menu, open) => {
-				if (open) {
+				//~ if (open) {
+				if (false) {
 					this._clipboard.get_text(St.ClipboardType.PRIMARY, (clipboard, text) => {
 						if (text && text.length > 4 && text !== this.lasttext) {
 							const r = GLib.find_program_in_path("qrencode");
@@ -61,14 +76,13 @@ const Indicator = GObject.registerClass(
 					try {
 						let [, stdout, stderr] = proc.communicate_finish(res);
 						if (proc.get_successful()) {
-							if (!GLib.file_test(tmpfile, GLib.FileTest.IS_REGULAR)){
-								lg("no file.");
-								return;
-							}
-							lg("out: " + stdout);
-							Gio.app_info_launch_default_for_uri(`file://${tmpfile}`, global.create_app_launch_context(0, -1));
+							//~ if (!GLib.file_test(tmpfile, GLib.FileTest.IS_REGULAR)){
+								//~ lg("no file.");
+								//~ return;
+							//~ }
+							//~ Gio.app_info_launch_default_for_uri(`file://${tmpfile}`, global.create_app_launch_context(0, -1));
 							this.icon.set_gicon(Gio.icon_new_for_string(tmpfile));
-							//~ 不更新显示？??????
+							//~ cp xxx.png qrcode.png, 点击刷新，按下是新图片，松手变回第一个。
 						}
 					} catch (e) { logError(e); }
 				});
